0017c8c94f56709afb329a7b56873045090dc404
HAI <guohai@gmail.com>
2019-07-14 21:40:45 +0800

    增加了两个页面
    1. 计算页面
    2. 结果页面

--- src/main/java/org/guohai/vaccine/ui/CalculatorController.java
+++ src/main/java/org/guohai/vaccine/ui/CalculatorController.java
@@ -1,13 +1,61 @@
 package org.guohai.vaccine.ui;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+
+@Controller
 public class CalculatorController {
+    @Autowired
+    private HttpServletRequest request;
 
     @RequestMapping(value = "/")
-    public String  index() {
-        return "aa";
+    public String  index(Model model) {
+
+        model.addAttribute("user",getIpAddr(request));
+        return "index";
     }
+
+    @RequestMapping(value = "/result")
+    public String result(Model model) {
+        return "result";
+    }
+
+    private String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+            if(ip.equals("127.0.0.1")){
+                //根据网卡取本机配置的IP
+                InetAddress inet=null;
+
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                ip= inet.getHostAddress();
+            }
+        }
+        // 多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if(ip != null && ip.length() > 15){
+            if(ip.indexOf(",")>0){
+                ip = ip.substring(0,ip.indexOf(","));
+            }
+        }
+        return ip;
+    }
+
 }
