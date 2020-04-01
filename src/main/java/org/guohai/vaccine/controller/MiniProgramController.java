1bc693b659f85d9cccb891a06b44f8a4719b4a6f
guohai <guohai@gmail.com>
2020-04-01 16:19:39 +0800

    add cross

--- src/main/java/org/guohai/vaccine/controller/MiniProgramController.java
+++ src/main/java/org/guohai/vaccine/controller/MiniProgramController.java
@@ -4,22 +4,28 @@ import org.guohai.vaccine.beans.Result;
 import org.guohai.vaccine.beans.VaccineBatchBean;
 import org.guohai.vaccine.service.MiniProgramService;
 import org.guohai.vaccine.service.VaccineBatchService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.InputStream;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/mini")
 public class MiniProgramController {
-
+    private static final Logger LOG  = LoggerFactory.getLogger(MiniProgramController.class);
     @Autowired
     MiniProgramService miniProgramService;
 
     @Autowired
     VaccineBatchService vaccineBatchService;
+
+    @Autowired
+    private HttpServletRequest request;
 
     @RequestMapping(value = "/oalogin")
     public Result<String> oalogin(@ModelAttribute("code") String code,@ModelAttribute("src") String src) {
@@ -38,5 +44,12 @@ public class MiniProgramController {
         return vaccineBatchService.searchVaccineBatch(vackey, loginCode, formId,userIp , userAgent);
     }
 
+    @ResponseBody
+    @RequestMapping(value = "/getlast")
+    @CrossOrigin
+    public Result<String> getDataLast() {
+        LOG.info("user in,ip:"+request.getRemoteAddr()+",UA:"+request.getHeader("User-Agent"));
+        return vaccineBatchService.getLastDate();
+    }
 
 }
